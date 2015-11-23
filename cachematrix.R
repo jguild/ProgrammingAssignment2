04f9bb648e18d706e1e3d3b0b63ff8c4dab0c83a
Jeff Guild <jguild10@gmail.com>
2015-11-22 18:24:04 -0800

    Assignment 2
    I finished this at noon and thought I had until 11:30 PM at some place
    in North America. Now I know what UTC means.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the Cashing the Inverse of a Matrix asignment (#2) from R Programming
+## due the end of week 3 (11/25/2015) There are two functions below that are
+## intended to demonstrate an understanding of "cashe"ing a variable so it will
+## be  and returned to the current environment in lieu of being recalculated
 
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its inverse and return the value
+makeCacheMatrix <- function (x = matrix()) {
+  x_inv <<- solve(x)
+  #x_inv <- function(solve) x
+  return(x_inv)
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Return a matrix that a previously cached inverse value of 'x' from outside
+## the environment if it exists. Note that if you specify a new 'X' and a cached
+## value is found, the value returned will not be correct
+cacheSolve <- function (x,...) {
+    if(!is.null(x_inv)){
+    return (x_inv)
+  }
+  x_inv <- solve(x)
+    return (x_inv)
+ }
